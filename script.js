9eb10f92b0b0c2df65c79aed0e538cbd58721119
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-14 20:59:54 -0500

    Objectives proposed

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+Salary Operations
+
+This program will give us data about the people income by country.
+-Each country will have its person list.
+-Each person has to have his/her own name and money income.
+-Each person will be created as an object, the only 2 data they will tell is the name and the monthly income.
+
+//1. Calculate the mode for each country
+//2. Calculate the median.
+//3. Calculate the mode.
+NOTE: Each calculation has to be done for the 10% most wealthy people and the 90% left. 
+*/
