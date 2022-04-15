76b70f3dfe197bc673771ff24ebd54ac18953821
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-14 21:46:20 -0500

    Little changes

--- scriptAnalisis.js
+++ scriptAnalisis.js
@@ -1,1 +1,17 @@
-console.log("wedqwefq");
+/*
+Salary Operations
+
+This program will give us data about the people income by country.
+-Each country will have its person list.
+-Each person has to have his/her own name and money income.
+-Each person will be created as an object, the only 2 data they will tell is the name and the monthly income.
+
+* Calculate the mode for each country
+* Calculate the median.
+* Calculate the mode.
+NOTE: Each calculation has to be done for the 10% most wealthy people and the 90% left. 
+
+//1. Create the first country list.
+//2.Calculate the median.
+*/
+
--- scriptSalary.js
+++ scriptSalary.js
@@ -1,17 +1,5 @@
-/*
-Salary Operations
 
-This program will give us data about the people income by country.
--Each country will have its person list.
--Each person has to have his/her own name and money income.
--Each person will be created as an object, the only 2 data they will tell is the name and the monthly income.
-
-* Calculate the mode for each country
-* Calculate the median.
-* Calculate the mode.
-NOTE: Each calculation has to be done for the 10% most wealthy people and the 90% left. 
-*/
-
+//1.
 const colombia = [];
 
 colombia.push({
