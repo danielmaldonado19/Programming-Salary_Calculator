e1511b5d023bef2c7514948440cc90773153b54f
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-14 21:34:41 -0500

    Colombia's people created

---
+++ scriptSalary.js
@@ -0,0 +1,124 @@
+/*
+Salary Operations
+
+This program will give us data about the people income by country.
+-Each country will have its person list.
+-Each person has to have his/her own name and money income.
+-Each person will be created as an object, the only 2 data they will tell is the name and the monthly income.
+
+* Calculate the mode for each country
+* Calculate the median.
+* Calculate the mode.
+NOTE: Each calculation has to be done for the 10% most wealthy people and the 90% left. 
+*/
+
+const colombia = [];
+
+colombia.push({
+    name: "Juan Cardona",
+    sector: "Minerals",
+    salary: 3500000,
+});
+colombia.push({
+    name: "Juliana Granda",
+    sector: "Minerals",
+    salary: 2700000,
+});
+colombia.push({
+    name: "Daniel Maldonado",
+    sector: "Minerals",
+    salary: 27000000,
+});
+colombia.push({
+    name: "Elkin Torres",
+    sector: "Minerals",
+    salary: 12000000,
+});
+colombia.push({
+    name: "Sofia Gomez",
+    sector: "Minerals",
+    salary: 3600000,
+});
+colombia.push({
+    name: "Daniela Fabra",
+    sector: "Minerals",
+    salary: 4500000,
+});
+colombia.push({
+    name: "Pedro Castano",
+    sector: "Minerals",
+    salary: 4500000,
+});
+colombia.push({
+    name: "Esperanza Juanes",
+    sector: "Minerals",
+    salary: 2600000,
+});
+colombia.push({
+    name: "Marta Martinez",
+    sector: "Minerals",
+    salary: 2700000,
+});
+colombia.push({
+    name: "Tatiana Faena",
+    sector: "Minerals",
+    salary: 3900000,
+});
+
+colombia.push({
+    name: "Andres Villa",
+    sector: "Vegetal Derived",
+    salary: 4500000,
+});
+colombia.push({
+    name: "Sandra Rojas",
+    sector: "Vegetal Derived",
+    salary: 2800000,
+});
+colombia.push({
+    name: "Fabio Sanchez",
+    sector: "Vegetal Derived",
+    salary: 2000000,
+});
+
+colombia.push({
+    name: "Viviana Pardo",
+    sector: "Textils",
+    salary: 3000000,
+});
+
+colombia.push({
+    name: "David Osorio",
+    sector: "Chemicals",
+    salary: 5400000,
+});
+
+colombia.push({
+    name: "Felipe Arias",
+    sector: "Metals",
+    salary: 6000000,
+});
+
+colombia.push({
+    name: "Beatriz Ochoa",
+    sector: "Machinery",
+    salary: 5500000,
+});
+
+colombia.push({
+    name: "Oscar Enriquez",
+    sector: "Foods",
+    salary: 4500000,
+});
+
+colombia.push({
+    name: "Salome Vives",
+    sector: "Plastics",
+    salary: 3900000,
+});
+
+colombia.push({
+    name: "Ana Maria Lopez",
+    sector: "Transport",
+    salary: 3400000,
+});
