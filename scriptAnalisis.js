5762f64090382f63ecf0b17462c56a6394d0ebaf
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-16 16:38:36 -0500

    Median working

--- scriptAnalisis.js
+++ scriptAnalisis.js
@@ -6,7 +6,6 @@ This program will give us data about the people income by country.
 -Each person has to have his/her own name and money income.
 -Each person will be created as an object, the only 2 data they will tell is the name and the monthly income.
 
-* Calculate the mode for each country
 * Calculate the median.
 * Calculate the mode.
 NOTE: Each calculation has to be done for the 10% most wealthy people and the 90% left. 
@@ -15,3 +14,24 @@ NOTE: Each calculation has to be done for the 10% most wealthy people and the 90
 //2.Calculate the median.
 */
 
+//2.
+var orderedList = colombia.sort(compare);
+function compare(a,b){
+    let comparation = a.salary - b.salary;
+    return comparation;
+}
+
+function median(colombia){
+    let subIndex = parseInt(colombia.length / 2);
+    let medianNum1 = colombia[subIndex].salary;
+    let medianNum2 = colombia[subIndex - 1].salary;
+    let median;
+    
+    if(colombia.length % 2 == 0){
+        median = (medianNum1 + medianNum2) / 2;
+    }
+    else{
+        median = colombia[subIndex];
+    }
+    return median;
+}
